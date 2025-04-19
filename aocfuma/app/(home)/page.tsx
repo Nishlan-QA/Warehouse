297b56663881025a33d69a7e02229254acc97df0
Nishlan-QA <nishlancbe@gmail.com>
2025-04-19 14:38:40 +0530

    warehouse project documentation

---
+++ aocfuma/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen text-center px-4">
+      <h1 className="text-4xl font-bold mb-4">🏭 Welcome to WareFlex</h1>
+      <p className="text-lg text-gray-600 max-w-xl">
+        Smart Warehouse Booking & Logistics Management System. 
+        Discover, Book, and Manage warehouse spaces easily. 🚚📦
+      </p>
+
+      
+    </main>
+  )
+}
